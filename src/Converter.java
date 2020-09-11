354914df4ecf99b26d05539b62a3882e1a57123d
justin-leong <leonj@ucalgary.ca>
2020-09-11 12:04:41 -0600

    Created Converter.java file

---
+++ src/Converter.java
@@ -0,0 +1,26 @@
+public class Converter {
+	//Your names go here:
+	/*
+	* @Author: Name of the first student
+	* Name of the second student
+	* Name of the third student
+	*
+	*/
+	private double celsiusToFahrenheit(double C){
+		// TODO: The third student will implement this method
+		return 0;
+	}
+	
+	private double fahrenheitToCelsius(double F){
+		// TODO: The second student will implement this method
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+	//TODO: The first student will implement this method.
+	// Call CelsiusToFahrenheit to convert 180 Celsius to Fahrenheit value.
+	// Call FahrenheitToCelsius to convert 250 Fahrenheit to Celsius value.
+		
+	
+	}
+}
